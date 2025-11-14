cb489b7ac2edc661f688b362d873af878493579f
Lewis Thomson <lewisthomson1989@gmail.com>
2025-11-14 15:46:13 +0000

    3D_graph

--- python_dev/OpenMeteo/main.py
+++ python_dev/OpenMeteo/main.py
@@ -128,6 +128,7 @@ dates = sorted(dates)
 x = np.arange(len(dates))
 
 num_cities = len(cities)
+num_dates = len(dates)
 group_gap = 0.2
 
 bar_width = (1 -  group_gap) / num_cities
@@ -139,6 +140,7 @@ plt.figure(figsize=(12, 6))
 for city_index, city in enumerate(cities):  
     group_data = WEATHER.xs(city, level="city").reindex(dates)
     print(group_data)
+    
     #group_data = group_data.reindex(dates)
     offset = (city_index - (num_cities - 1) / 2) * bar_width
     avg_temp = (group_data['temp_max'] + group_data['temp_min']) / 2
@@ -166,9 +168,22 @@ plt.show()
 
 
 plt.axes(projection='3d')
-width = depth = bar_width
 
-plt.bar(x + offset, num_cities, bottom=group_data['temp_min'])
+x_pos = np.arange(num_dates)
+y_pos = np.arange(num_cities)
+
+for city_index, city in enumerate(cities):  
+    group_data = WEATHER.xs(city, level="city").reindex(dates)
+
+    z_values = group_data['temp_max'].values
+    ys = np.full(num_dates, city_index)
+    z_base = np.zeros(num_dates)
+
+dx = 0.5
+dy = 0.5
+dz = z_values
+
+plt.bar(x_pos, ys)
 
 
 plt.xticks(x, dates)
